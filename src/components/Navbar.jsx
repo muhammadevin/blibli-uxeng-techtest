fc812feac2a832d1e9b941d05525dec1451be00c
Devin Pratista <devinpratista@Devins-MacBook-Air.local>
2023-07-12 00:31:33 +0700

    Edit logo position in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../public/blibli.png'
 
 const Navbar = () => {
   return (
-    <div className='bg-primary flex justify-center items-center py-4'>
+    <div className='bg-primary flex justify-center items-center md:justify-start px-10 py-4'>
       <img src={logo} alt="logo" className='w-[75px] h-auto'/>
     </div>
   )
